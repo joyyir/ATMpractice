bf636c5061c8a5f8cce3fc430917fecc62724a0c
Jun-yeong Jang <joyyir@naver.com>
2016-06-25 16:36:57 +0900

    replacing Activity to Fragment (not complete)

--- app/src/main/java/joyyir/atmpractice/MainActivity.java
+++ app/src/main/java/joyyir/atmpractice/MainActivity.java
@@ -1,13 +1,35 @@
 package joyyir.atmpractice;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    static public MainActivity self;
+
+    static public MainActivity getInstance(){
+        return self;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        self = this;
+
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new MainFragment();
+        replaceFragment(R.id.ll_fragment_atm, fragment);
+    }
+
+    public void replaceFragment(int fragAreaId, Fragment newFragment) {
+        // replace fragment
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(fragAreaId, newFragment);
+
+        // Commit the transaction
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/joyyir/atmpractice/MainFragment.java
@@ -0,0 +1,28 @@
+package joyyir.atmpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button btnTransfer = (Button)view.findViewById(R.id.btnMainTransfer);
+        btnTransfer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                MainActivity.getInstance().replaceFragment(R.id.ll_fragment_atm, new ReadingCardFragment());
+            }
+        });
+
+        return view;
+        //return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
